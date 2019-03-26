22890ed91e3f0893a34fea64c17bb2fa8172787e
tuftman-0 <28765011+tuftman-0@users.noreply.github.com>
2019-03-25 21:36:15 -0400

    broken right now

--- bot.py
+++ bot.py
@@ -1,6 +1,19 @@
 import discord
 import time, datetime
 import json
+import asyncio
+
+def str2time(s):
+    a,b = map(int,s.split(':'))
+    return a,b
+
+def time2str(t):
+    return '%0.2d:%0.2d' % t
+
+def timediff(t, t0):
+    h, m   = t
+    h0, m0 = t0
+    return (h-h0)%12, (m-m0)%6
 
 client = discord.Client()
 
@@ -13,20 +26,30 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content == "Hello":
-        # await client.send_message(message.channel, "World")
-        await message.channel.send("World")
+    if message.content == "!time":
+        current = time.strftime('%I:%M')
+        two_to  = time2str(timediff(str2time(current),(0,-2)))
+        await message.channel.send("my clock says two to {0} does your clock say two to {0} too".format(two_to))
 
 
-@client.event
-async def time_check():
-    await client.wait_until_ready()
-    counter = 0
-    channel = 542493356266356736
+# @client.event
+# async def time_check():
+#     await client.wait_until_ready()
+#     counter = 0
+#     channel = discord.Object(id='542493356266356736')
+#     trigger = '2:00'
+#     while not client.is_closed:
+#         current = time.strftime('%I:%M')
+#         hour, minute = map(int,current.split(':'))
+#         if 60 - minute <= 3:
+#             if current == trig
+#                 message.channel.send("my clock says two to {0} does your clock say two to {0} too".format(trigger)
+#         else:
+#             asyncio.sleep(60)
     
 
 # get token from file so you don't accidentally reveal it in your github repository
 with open('token.txt','r') as f:
-    token = f.read()
-
-client.run(token)
+    token = str(f.read())
+    print(token)
+    client.run(token)
