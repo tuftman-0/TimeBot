6159e0fe0ba73c5c7be58de1b00b763179ec8e6c
tuftman-0 <28765011+tuftman-0@users.noreply.github.com>
2019-03-25 22:30:50 -0400

    squashed the bug I was looking for

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import discord
-import time, datetime
+import time
 import json
-import asyncio
 
 def str2time(s):
     a,b = map(int,s.split(':'))
@@ -13,43 +12,27 @@ def time2str(t):
 def timediff(t, t0):
     h, m   = t
     h0, m0 = t0
-    return (h-h0)%12, (m-m0)%6
+    return (h-h0)%12, (m-m0)%60
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print("The bot is ready!")
-    await client.change_presence(activity=discord.Game(name="Making a bot"))
+    await client.change_presence(activity=discord.Game(name="with time"))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     if message.content == "!time":
-        current = time.strftime('%I:%M')
-        two_to  = time2str(timediff(str2time(current),(0,-2)))
+        current = time.strftime("%I:%M")
+        two_to  = time2str(timediff(str2time(current), (0,-2)))
         await message.channel.send("my clock says two to {0} does your clock say two to {0} too".format(two_to))
 
 
-# @client.event
-# async def time_check():
-#     await client.wait_until_ready()
-#     counter = 0
-#     channel = discord.Object(id='542493356266356736')
-#     trigger = '2:00'
-#     while not client.is_closed:
-#         current = time.strftime('%I:%M')
-#         hour, minute = map(int,current.split(':'))
-#         if 60 - minute <= 3:
-#             if current == trig
-#                 message.channel.send("my clock says two to {0} does your clock say two to {0} too".format(trigger)
-#         else:
-#             asyncio.sleep(60)
-    
-
 # get token from file so you don't accidentally reveal it in your github repository
-with open('token.txt','r') as f:
-    token = str(f.read())
-    print(token)
+with open('config.json','r') as f:
+    data = json.load(f)
+    token = data['token']
     client.run(token)
